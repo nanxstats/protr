11be74b7116d5d23b40240007343ac9fae5dceec
Xiao Nan <road2stat@gmail.com>
2012-11-05 20:43:41 +0800

    first commit package skeleton

---
+++ R/rdpi-package.R
@@ -0,0 +1,49 @@
+#' Protein Sequence Feature Extraction with R
+#'
+#' The rdpi package aims for protein sequence feature extraction, 
+#' which could be easily applied in Chemoinformatics, Bioinforamtics 
+#' and Chemogenomics research. The features listed in this package 
+#' include Amino Acid Composition (Amino Acid Composition/Dipeptide 
+#' Composition/Tripeptide Composition), Autocorrelation (Normalized 
+#' Moreau-Broto Autocorrelation/Moran Autocorrelation/Geary 
+#' Autocorrelation), CTD (Composition/Transition/Distribution), 
+#' Conjoint Traid, Quasi-sequence Order (Sequence Order Coupling 
+#' Number/Quasi-sequence Order Descriptors), and Pseudo Amino Acid 
+#' Composition (Pseudo Amino Acid Composition/Amphiphilic Pseudo 
+#' Amino Acid Composition). The package is developed by Computational 
+#' Biology and Drug Design Group, Central South University.
+#'
+#' \tabular{ll}{ Package: \tab rdpi\cr Type: \tab Package\cr
+#' Version: \tab 0.1-0\cr License: \tab BSD\cr }
+#'
+#' @section Amino Acid Composition: put AAC description here.
+#'
+#' @section Dipeptide Composition: put DC description here.
+#'
+#' @name rdpi-package
+#' @aliases rdpi
+#' @docType package
+#' @exportPattern "^[^\\.]"
+#' @author Xiao Nan <\url{http://www.road2stat.com}>
+#'         Dongsheng Cao \email{oriental-cds@@163.com}
+#'         Qingsong Xu \email{dasongxu@@gmail.com}
+#'         Yizeng Liang \email{yizeng_liang@@263.net}
+#' 
+#' @note
+#' Bug reports and feature requests should be sent to
+#' \url{https://github.com/road2stat/rdpi/issues}.
+#' 
+#' @references
+#' The associated website for this package:
+#' \url{http://cbdd.csu.edu.cn/rdpi/}
+#'
+#' @seealso Add \code{\link{rca}}, \code{\link{lfda}}, 
+#'              \code{\link{klfda}}, \code{\link{dca}},
+#'              \code{\link{kdca}}, etc.
+#' 
+#' @keywords package protein sequence amino acid feature extraction descriptors
+#'           chemoinformatics bioinforamtics chemogenomics
+#' 
+#' @example inst/examples/rdpi-package-ex.R
+NULL
+
---
+++ README.md
@@ -0,0 +1,58 @@
+# rdpi
+
+## Introduction
+
+The rdpi package aims for protein sequence feature extraction, which could be easily applied in Chemoinformatics, Bioinforamtics and Chemogenomics research. The package is developed by Computational Biology and Drug Design Group, Central South University.
+
+## Protein Sequence Features
+
+The protein sequence features listed in this package include
+
+  * Amino Acid Composition 
+  
+    * Amino Acid Composition
+    * Dipeptide Composition
+    * Tripeptide Composition
+
+  * Autocorrelation
+  
+    * Normalized Moreau-Broto Autocorrelation
+    * Moran Autocorrelation
+    * Geary Autocorrelation
+
+  * CTD
+  
+    * Composition
+    * Transition
+    * Distribution
+
+  * Conjoint Traid
+
+  * Quasi-sequence Order
+  
+    * Sequence Order Coupling Number
+    * Quasi-sequence Order Descriptors
+  
+  * Pseudo Amino Acid Composition
+  
+    * Pseudo Amino Acid Composition
+    * Amphiphilic Pseudo Amino Acid Composition
+
+## Links
+
+  * CRAN Page: http://cran.r-project.org/web/packages/rdpi/
+
+  * Track Devel: https://github.com/road2stat/rdpi
+
+  * Report Bugs: https://github.com/road2stat/rdpi/issues
+
+## Contact
+
+  * Xiao Nan <road2stat@gmail.com>
+
+  * Dongsheng Cao <oriental-cds@163.com>
+
+  * Qingsong Xu <dasongxu@gmail.com>
+
+  * Yizeng Liang <yizeng_liang@263.net>
+
