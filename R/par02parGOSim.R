e215e805cca6a60fd88b75a99cfb6a9efaf64960
Xiao Nan <road2stat@gmail.com>
2013-12-16 09:58:43 +0800

    finishing vignette

--- R/par02parGOSim.R
+++ R/par02parGOSim.R
@@ -17,12 +17,8 @@
     
     res = try(suppressWarnings(GOSemSim::mgoSim(gid1, gid2, ont = ont, organism = organism, measure = measure, combine = combine)), silent = TRUE)
     
-    if ( is.list(res) ) {
-      if ( res$geneSim == Inf ) {
-        sim = 1L
-        } else {
-          sim = res$geneSim
-        }
+    if ( is.numeric(res) ) {
+      sim = res
       } else {
         sim = 0L
       }
@@ -33,11 +29,11 @@
   
 }
 
-#' Parallellized Protein Sequence Similarity Calculation based on Gene Ontology (GO) Similarity
+#' Protein Sequence Similarity Calculation based on Gene Ontology (GO) Similarity
 #'
-#' Parallellized Protein Sequence Similarity Calculation based on Gene Ontology (GO) Similarity
+#' Protein Sequence Similarity Calculation based on Gene Ontology (GO) Similarity
 #'
-#' This function implemented the parallellized version for calculating 
+#' This function calculates
 #' protein sequence similarity based on Gene Ontology (GO) similarity.
 #' 
 #' @param golist A character vector, each component contains
@@ -56,7 +52,7 @@
 #' 
 #' @note Note
 #' 
-#' @keywords GO Gene Ontology parallel similarity parGOSim
+#' @keywords GO Gene Ontology similarity parGOSim
 #'
 #' @aliases parGOSim
 #' 
@@ -70,20 +66,20 @@
 #' @examples
 #' \dontrun{
 #' # by GO Terms
-#' go1 = c("GO:0004022", "GO:0004024", "GO:0004023")
-#' go2 = c("GO:0009055", "GO:0020037")
-#' go3 = c("GO:0020039", "GO:0009056")
+#' go1 = c('GO:0005215', 'GO:0005488', 'GO:0005515', 'GO:0005625', 'GO:0005802', 'GO:0005905')  # AP4B1
+#' go2 = c('GO:0005515', 'GO:0005634', 'GO:0005681', 'GO:0008380', 'GO:0031202')  # BCAS2
+#' go3 = c('GO:0003735', 'GO:0005622', 'GO:0005840', 'GO:0006412')  # PDE4DIP
 #' glist = list(go1, go2, go3)
-#' gsimmat1 = parGOSim(glist, type = 'go', cores = 2)
+#' gsimmat1 = parGOSim(glist, type = 'go', ont = 'CC')
 #' print(gsimmat1)
+#' 
 #' # by Entrez gene id
-#' genelist = list("835", "5261", "241")
-#' gsimmat2 = parGOSim(genelist, type = 'gene', cores = 2)
+#' genelist = list(c('150', '151', '152', '1814', '1815', '1816'))
+#' gsimmat2 = parGOSim(genelist, type = 'gene')
 #' print(gsimmat2) }
 #' 
 
 parGOSim = function (golist, type = c('go', 'gene'), 
-                     cores = 2, 
                      ont = 'MF', organism = 'human', 
                      measure = 'Resnik', combine = 'BMA') {
   
@@ -96,36 +92,14 @@ parGOSim = function (golist, type = c('go', 'gene'),
   
   if ( type == 'go' ) {
     
-    foreach.exist = suppressMessages(require(foreach, quietly = TRUE))
-    doParallel.exist = suppressMessages(require(doParallel, quietly = TRUE))
-    doMC.exist = suppressMessages(require(doMC, quietly = TRUE))
-    
-    if ( (foreach.exist) & (doParallel.exist | doMC.exist) ) {
-      if( !getDoParRegistered() ) {
-        if ( .Platform$OS.type == 'windows' & doParallel.exist ) {
-          registerDoParallel(cores)
-        } else if ( .Platform$OS.type == 'unix' & doMC.exist ) {
-          registerDoMC(cores)
-        } else if ( .Platform$OS.type == 'unix' & doParallel.exist ) {
-          registerDoParallel(cores)
-        } else {
-          stop('doParallel or doMC is required to run parGOSim()')
-        }
-      }
-    } else {
-      stop('foreach and doParallel/doMC is required to run parGOSim()')
-    }
-    
     # generate lower matrix index
     idx = combn(1:length(golist), 2)
     
-    # then use foreach parallelization
     # input is all pair combination
-    
     gosimlist = vector('list', ncol(idx))
     
-    gosimlist <- foreach (i = 1:ncol(idx), .errorhandling = 'pass') %dopar% {
-      tmp <- .goPairSim(rev(idx[, i]), golist = golist, ont = ont, organism = organism, measure = measure, combine = combine)
+    for ( i in 1:ncol(idx) ) {
+      gosimlist[[i]] = .goPairSim(rev(idx[, i]), golist = golist, ont = ont, organism = organism, measure = measure, combine = combine)
     }
     
     # convert list to matrix
@@ -183,6 +157,7 @@ parGOSim = function (golist, type = c('go', 'gene'),
 #' go2 = c("GO:0009055", "GO:0020037")
 #' gsim1 = twoGOSim(go1, go2, type = 'go', ont = 'MF', measure = 'Wang')
 #' print(gsim1)
+#' 
 #' # by Entrez gene id
 #' gene1 = '241'
 #' gene2 = '251'
