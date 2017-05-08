9f7f7f522757b5d7ffb4dbf2da5ab65bfe06ce9a
Nan Xiao <road2stat@gmail.com>
2017-05-07 20:46:12 -0400

    reformat similarity doc

--- R/par-01-parSeqSim.R
+++ R/par-01-parSeqSim.R
@@ -1,9 +1,11 @@
-.seqPairSim = function (twoid, protlist = protlist, type = type, submat = submat) {
+.seqPairSim = function(
+  twoid, protlist = protlist, type = type, submat = submat) {
 
   id1 = twoid[1]
   id2 = twoid[2]
 
-  if ( protlist[[id1]] == '' | protlist[[id2]] == '' ) {
+  if ( protlist[[id1]] == '' |
+       protlist[[id2]] == '' ) {
 
     sim = 0L
 
@@ -11,13 +13,22 @@
 
     s1  = try(Biostrings::AAString(protlist[[id1]]), silent = TRUE)
     s2  = try(Biostrings::AAString(protlist[[id2]]), silent = TRUE)
-    s12 = try(Biostrings::pairwiseAlignment(s1, s2, type = type, substitutionMatrix = submat, scoreOnly = TRUE), silent = TRUE)
-    s11 = try(Biostrings::pairwiseAlignment(s1, s1, type = type, substitutionMatrix = submat, scoreOnly = TRUE), silent = TRUE)
-    s22 = try(Biostrings::pairwiseAlignment(s2, s2, type = type, substitutionMatrix = submat, scoreOnly = TRUE), silent = TRUE)
+    s12 = try(Biostrings::pairwiseAlignment(
+      s1, s2, type = type, substitutionMatrix = submat, scoreOnly = TRUE),
+      silent = TRUE)
+    s11 = try(Biostrings::pairwiseAlignment(
+      s1, s1, type = type, substitutionMatrix = submat, scoreOnly = TRUE),
+      silent = TRUE)
+    s22 = try(Biostrings::pairwiseAlignment(
+      s2, s2, type = type, substitutionMatrix = submat, scoreOnly = TRUE),
+      silent = TRUE)
 
-    if ( is.numeric(s12) == FALSE | is.numeric(s11) == FALSE | is.numeric(s22) == FALSE ) {
+    if ( is.numeric(s12) == FALSE |
+         is.numeric(s11) == FALSE |
+         is.numeric(s22) == FALSE ) {
       sim = 0L
-    } else if ( abs(s11) < .Machine$double.eps | abs(s22) < .Machine$double.eps ) {
+    } else if ( abs(s11) < .Machine$double.eps |
+                abs(s22) < .Machine$double.eps ) {
       sim = 0L
     } else {
       sim = s12/sqrt(s11 * s22)
@@ -25,28 +36,30 @@
 
   }
 
-  return(sim)
+  sim
 
 }
 
-#' Parallellized Protein Sequence Similarity Calculation based on Sequence Alignment
+#' Parallellized Protein Sequence Similarity Calculation based on
+#' Sequence Alignment
 #'
 #' This function implemented the parallellized version for calculating
 #' protein sequence similarity based on sequence alignment.
 #'
 #' @param protlist A length \code{n} list containing \code{n} protein sequences,
-#' each component of the list is a character string, storing one protein sequence.
-#' Unknown sequences should be represented as \code{''}.
+#' each component of the list is a character string, storing one protein
+#' sequence. Unknown sequences should be represented as \code{""}.
 #' @param cores Integer. The number of CPU cores to use for parallel execution,
-#'        default is \code{2}. Users could use the \code{detectCores()} function
-#'        in the \code{parallel} package to see how many cores they could use.
+#' default is \code{2}. Users could use the \code{detectCores()} function
+#' in the \code{parallel} package to see how many cores they could use.
 #' @param type Type of alignment, default is \code{'local'},
 #' could be \code{'global'} or \code{'local'},
 #' where \code{'global'} represents Needleman-Wunsch global alignment;
 #' \code{'local'} represents Smith-Waterman local alignment.
-#' @param submat Substitution matrix, default is \code{'BLOSUM62'}, could be one of
-#' \code{'BLOSUM45'}, \code{'BLOSUM50'}, \code{'BLOSUM62'}, \code{'BLOSUM80'}, \code{'BLOSUM100'},
-#' \code{'PAM30'}, \code{'PAM40'}, \code{'PAM70'}, \code{'PAM120'}, \code{'PAM250'}.
+#' @param submat Substitution matrix, default is \code{'BLOSUM62'},
+#' can be one of \code{'BLOSUM45'}, \code{'BLOSUM50'}, \code{'BLOSUM62'},
+#' \code{'BLOSUM80'}, \code{'BLOSUM100'}, \code{'PAM30'},
+#' \code{'PAM40'}, \code{'PAM70'}, \code{'PAM120'}, or \code{'PAM250'}.
 #'
 #' @return A \code{n} x \code{n} similarity matrix.
 #'
@@ -68,20 +81,21 @@
 #' # Be careful when testing this since it involves parallelisation
 #' # and might produce unpredictable results in some environments
 #'
-#' require(Biostrings)
-#' require(foreach)
-#' require(doParallel)
+#' library("Biostrings")
+#' library("foreach")
+#' library("doParallel")
 #'
-#' s1 = readFASTA(system.file('protseq/P00750.fasta', package = 'protr'))[[1]]
-#' s2 = readFASTA(system.file('protseq/P08218.fasta', package = 'protr'))[[1]]
-#' s3 = readFASTA(system.file('protseq/P10323.fasta', package = 'protr'))[[1]]
-#' s4 = readFASTA(system.file('protseq/P20160.fasta', package = 'protr'))[[1]]
-#' s5 = readFASTA(system.file('protseq/Q9NZP8.fasta', package = 'protr'))[[1]]
+#' s1 = readFASTA(system.file("protseq/P00750.fasta", package = "protr"))[[1]]
+#' s2 = readFASTA(system.file("protseq/P08218.fasta", package = "protr"))[[1]]
+#' s3 = readFASTA(system.file("protseq/P10323.fasta", package = "protr"))[[1]]
+#' s4 = readFASTA(system.file("protseq/P20160.fasta", package = "protr"))[[1]]
+#' s5 = readFASTA(system.file("protseq/Q9NZP8.fasta", package = "protr"))[[1]]
 #' plist = list(s1, s2, s3, s4, s5)
-#' psimmat = parSeqSim(plist, cores = 2, type = 'local', submat = 'BLOSUM62')
+#' psimmat = parSeqSim(plist, cores = 2, type = "local", submat = "BLOSUM62")
 #' print(psimmat)}
 
-parSeqSim = function (protlist, cores = 2, type = 'local', submat = 'BLOSUM62') {
+parSeqSim = function(
+  protlist, cores = 2, type = 'local', submat = 'BLOSUM62') {
 
   doParallel::registerDoParallel(cores)
 
@@ -95,17 +109,20 @@ parSeqSim = function (protlist, cores = 2, type = 'local', submat = 'BLOSUM62') 
 
   `%mydopar%` = foreach::`%dopar%`
 
-  seqsimlist <- foreach::foreach(i = 1:length(seqsimlist), .errorhandling = 'pass') %mydopar% {
-    tmp <- .seqPairSim(rev(idx[, i]), protlist = protlist, type = type, submat = submat)
-  }
+  seqsimlist <- foreach::foreach(
+    i = 1:length(seqsimlist), .errorhandling = 'pass') %mydopar% {
+      tmp <- .seqPairSim(
+        rev(idx[, i]), protlist = protlist, type = type, submat = submat)
+    }
 
   # convert list to matrix
   seqsimmat = matrix(0, length(protlist), length(protlist))
-  for (i in 1:length(seqsimlist)) seqsimmat[idx[2, i], idx[1, i]] = seqsimlist[[i]]
+  for (i in 1:length(seqsimlist))
+    seqsimmat[idx[2, i], idx[1, i]] = seqsimlist[[i]]
   seqsimmat[upper.tri(seqsimmat)] = t(seqsimmat)[upper.tri(t(seqsimmat))]
   diag(seqsimmat) = 1
 
-  return(seqsimmat)
+  seqsimmat
 
 }
 
@@ -119,11 +136,13 @@ parSeqSim = function (protlist, cores = 2, type = 'local', submat = 'BLOSUM62') 
 #' could be \code{'global'} or \code{'local'},
 #' where \code{'global'} represents Needleman-Wunsch global alignment;
 #' \code{'local'} represents Smith-Waterman local alignment.
-#' @param submat Substitution matrix, default is \code{'BLOSUM62'}, could be one of
-#' \code{'BLOSUM45'}, \code{'BLOSUM50'}, \code{'BLOSUM62'}, \code{'BLOSUM80'}, \code{'BLOSUM100'},
-#' \code{'PAM30'}, \code{'PAM40'}, \code{'PAM70'}, \code{'PAM120'}, \code{'PAM250'}.
+#' @param submat Substitution matrix, default is \code{'BLOSUM62'},
+#' can be one of \code{'BLOSUM45'}, \code{'BLOSUM50'}, \code{'BLOSUM62'},
+#' \code{'BLOSUM80'}, \code{'BLOSUM100'}, \code{'PAM30'}, \code{'PAM40'},
+#' \code{'PAM70'}, \code{'PAM120'}, or \code{'PAM250'}.
 #'
-#' @return An Biostrings object containing the scores and other alignment information.
+#' @return An Biostrings object containing the scores and other
+#' alignment information.
 #'
 #' @keywords alignment parallel similarity
 #'
@@ -133,8 +152,8 @@ parSeqSim = function (protlist, cores = 2, type = 'local', submat = 'BLOSUM62') 
 #'
 #' @seealso See \code{\link{parSeqSim}} for paralleled pairwise
 #' protein similarity calculation based on sequence alignment.
-#' See \code{\link{twoGOSim}} for calculating the
-#' GO semantic similarity between two groups of GO terms or two Entrez gene IDs.
+#' See \code{\link{twoGOSim}} for calculating the GO semantic
+#' similarity between two groups of GO terms or two Entrez gene IDs.
 #'
 #' @export twoSeqSim
 #'
@@ -142,24 +161,23 @@ parSeqSim = function (protlist, cores = 2, type = 'local', submat = 'BLOSUM62') 
 #' \donttest{
 #' # Be careful when testing this since it involves sequence alignment
 #' # and might produce unpredictable results in some environments
-#'
-#' require(Biostrings)
-#'
-#' s1 = readFASTA(system.file('protseq/P00750.fasta', package = 'protr'))[[1]]
-#' s2 = readFASTA(system.file('protseq/P10323.fasta', package = 'protr'))[[1]]
+#' library("Biostrings")
+#' s1 = readFASTA(system.file("protseq/P00750.fasta", package = "protr"))[[1]]
+#' s2 = readFASTA(system.file("protseq/P10323.fasta", package = "protr"))[[1]]
 #' seqalign = twoSeqSim(s1, s2)
 #' summary(seqalign)
 #' print(seqalign@@score)}
 
-twoSeqSim = function (seq1, seq2, type = 'local', submat = 'BLOSUM62') {
+twoSeqSim = function(
+  seq1, seq2, type = 'local', submat = 'BLOSUM62') {
 
   # sequence alignment for two protein sequences
   s1  = try(Biostrings::AAString(seq1), silent = TRUE)
   s2  = try(Biostrings::AAString(seq2), silent = TRUE)
-  s12 = try(Biostrings::pairwiseAlignment(s1, s2, type = type,
-                                          substitutionMatrix = submat),
-            silent = TRUE)
+  s12 = try(Biostrings::pairwiseAlignment(
+    s1, s2, type = type, substitutionMatrix = submat),
+    silent = TRUE)
 
-  return(s12)
+  s12
 
 }
--- R/par-02-parGOSim.R
+++ R/par-02-parGOSim.R
@@ -1,9 +1,12 @@
-.goPairSim = function (twoid, golist = golist, ont = ont, organism = organism, measure = measure, combine = combine) {
+.goPairSim = function(
+  twoid, golist = golist,
+  ont = ont, organism = organism, measure = measure, combine = combine) {
 
   id1 = twoid[1]
   id2 = twoid[2]
 
-  if ( all(golist[[id1]] == '') | all(golist[[id2]] == '') ) {
+  if ( all(golist[[id1]] == '') |
+       all(golist[[id2]] == '') ) {
 
     sim = 0L
 
@@ -15,12 +18,11 @@
     gid1 = as.character(golist[[id1]][id1good])
     gid2 = as.character(golist[[id2]][id2good])
 
-    res = try(suppressWarnings(GOSemSim::mgoSim(gid1, gid2,
-                                                ont = ont,
-                                                organism = organism,
-                                                measure = measure,
-                                                combine = combine)),
-              silent = TRUE)
+    res = try(suppressWarnings(
+      GOSemSim::mgoSim(
+        gid1, gid2,
+        ont = ont, organism = organism, measure = measure, combine = combine)),
+      silent = TRUE)
 
     if ( is.numeric(res) ) {
       sim = res
@@ -30,27 +32,36 @@
 
   }
 
-  return(sim)
+  sim
 
 }
 
-#' Protein Sequence Similarity Calculation based on Gene Ontology (GO) Similarity
+#' Protein Sequence Similarity Calculation based on Gene Ontology
+#' (GO) Similarity
 #'
 #' This function calculates protein sequence similarity based on
 #' Gene Ontology (GO) similarity.
 #'
 #' @param golist A character vector, each component contains
 #' a character vector of GO terms or one Entrez Gene ID.
-#' @param type Input type of \code{golist}, \code{'go'} for GO Terms, \code{'gene'} for gene ID.
-#' @param ont Default is \code{'MF'}, could be one of \code{'MF'}, \code{'BP'}, or \code{'CC'} subontologies.
-#' @param organism Default is \code{'human'}, could be one of \code{'anopheles'}, \code{'arabidopsis'}, \code{'bovine'}, \code{'canine'},
+#' @param type Input type of \code{golist}, \code{'go'} for GO Terms,
+#' \code{'gene'} for gene ID.
+#' @param ont Default is \code{'MF'}, could be one of \code{'MF'},
+#' \code{'BP'}, or \code{'CC'} subontologies.
+#' @param organism Default is \code{'human'}, could be one of
+#' \code{'anopheles'}, \code{'arabidopsis'}, \code{'bovine'}, \code{'canine'},
 #' \code{'chicken'}, \code{'chimp'}, \code{'coelicolor'}, \code{'ecolik12'},
 #' \code{'ecsakai'}, \code{'fly'}, \code{'human'}, \code{'malaria'},
 #' \code{'mouse'}, \code{'pig'}, \code{'rat'}, \code{'rhesus'},
 #' \code{'worm'}, \code{'xenopus'}, \code{'yeast'} or \code{'zebrafish'}.
-#' @param measure Default is \code{'Resnik'}, could be one of \code{'Resnik'}, \code{'Lin'}, \code{'Rel'}, \code{'Jiang'} or \code{'Wang'}.
-#' @param combine Default is \code{'BMA'}, could be one of \code{'max'}, \code{'average'}, \code{'rcmax'} or \code{'BMA'}
-#' for combining semantic similarity scores of multiple GO terms associated with protein.
+#' @param measure Default is \code{'Resnik'}, could be one of
+#' \code{'Resnik'}, \code{'Lin'}, \code{'Rel'}, \code{'Jiang'}
+#' or \code{'Wang'}.
+#' @param combine Default is \code{'BMA'}, could be one of \code{'max'},
+#' \code{'average'}, \code{'rcmax'} or \code{'BMA'}
+#' for combining semantic similarity scores of multiple GO terms
+#' associated with protein.
+#'
 #' @return A \code{n} x \code{n} similarity matrix.
 #'
 #' @keywords GO Ontology similarity
@@ -71,28 +82,34 @@
 #' # Be careful when testing this since it involves GO similarity computation
 #' # and might produce unpredictable results in some environments
 #'
-#' require(GOSemSim)
-#' require(org.Hs.eg.db)
+#' library("GOSemSim")
+#' library("org.Hs.eg.db")
 #'
 #' # by GO Terms
-#' go1 = c('GO:0005215', 'GO:0005488', 'GO:0005515', 'GO:0005625', 'GO:0005802', 'GO:0005905')  # AP4B1
-#' go2 = c('GO:0005515', 'GO:0005634', 'GO:0005681', 'GO:0008380', 'GO:0031202')  # BCAS2
-#' go3 = c('GO:0003735', 'GO:0005622', 'GO:0005840', 'GO:0006412')  # PDE4DIP
+#' go1 = c("GO:0005215", "GO:0005488", "GO:0005515",
+#'         "GO:0005625", "GO:0005802", "GO:0005905")  # AP4B1
+#' go2 = c("GO:0005515", "GO:0005634", "GO:0005681",
+#'         "GO:0008380", "GO:0031202")  # BCAS2
+#' go3 = c("GO:0003735", "GO:0005622", "GO:0005840",
+#'         "GO:0006412")  # PDE4DIP
 #' glist = list(go1, go2, go3)
-#' gsimmat1 = parGOSim(glist, type = 'go', ont = 'CC')
+#' gsimmat1 = parGOSim(glist, type = "go", ont = "CC")
 #' print(gsimmat1)
 #'
 #' # by Entrez gene id
-#' genelist = list(c('150', '151', '152', '1814', '1815', '1816'))
-#' gsimmat2 = parGOSim(genelist, type = 'gene')
+#' genelist = list(c("150", "151", "152", "1814", "1815", "1816"))
+#' gsimmat2 = parGOSim(genelist, type = "gene")
 #' print(gsimmat2)}
 
-parGOSim = function (golist, type = c('go', 'gene'),
-                     ont = 'MF', organism = 'human',
-                     measure = 'Resnik', combine = 'BMA') {
+parGOSim = function(
+  golist, type = c('go', 'gene'),
+  ont = 'MF', organism = 'human',
+  measure = 'Resnik', combine = 'BMA') {
 
   if ( type == 'gene' ) {
-    gosimmat = GOSemSim::mgeneSim(unlist(golist), ont = ont, organism = organism, measure = measure, combine = combine, verbose = FALSE)
+    gosimmat = GOSemSim::mgeneSim(
+      unlist(golist), ont = ont, organism = organism,
+      measure = measure, combine = combine, verbose = FALSE)
   }
 
   if ( type == 'go' ) {
@@ -104,7 +121,9 @@ parGOSim = function (golist, type = c('go', 'gene'),
     gosimlist = vector('list', ncol(idx))
 
     for ( i in 1:ncol(idx) ) {
-      gosimlist[[i]] = .goPairSim(rev(idx[, i]), golist = golist, ont = ont, organism = organism, measure = measure, combine = combine)
+      gosimlist[[i]] = .goPairSim(
+        rev(idx[, i]), golist = golist,
+        ont = ont, organism = organism, measure = measure, combine = combine)
     }
 
     # convert list to matrix
@@ -115,7 +134,7 @@ parGOSim = function (golist, type = c('go', 'gene'),
 
   }
 
-  return(gosimmat)
+  gosimmat
 
 }
 
@@ -128,16 +147,23 @@ parGOSim = function (golist, type = c('go', 'gene'),
 #' length = 1: the Entrez Gene ID.
 #' @param id2 A character vector. length > 1: each element is a GO term;
 #' length = 1: the Entrez Gene ID.
-#' @param type Input type of id1 and id2, \code{'go'} for GO Terms, \code{'gene'} for gene ID.
-#' @param ont Default is \code{'MF'}, could be one of \code{'MF'}, \code{'BP'}, or \code{'CC'} subontologies.
-#' @param organism Default is \code{'human'}, could be one of \code{'anopheles'}, \code{'arabidopsis'}, \code{'bovine'}, \code{'canine'},
+#' @param type Input type of id1 and id2, \code{'go'} for GO Terms,
+#' \code{'gene'} for gene ID.
+#' @param ont Default is \code{'MF'}, could be one of \code{'MF'},
+#' \code{'BP'}, or \code{'CC'} subontologies.
+#' @param organism Default is \code{'human'}, could be one of
+#' \code{'anopheles'}, \code{'arabidopsis'}, \code{'bovine'}, \code{'canine'},
 #' \code{'chicken'}, \code{'chimp'}, \code{'coelicolor'}, \code{'ecolik12'},
 #' \code{'ecsakai'}, \code{'fly'}, \code{'human'}, \code{'malaria'},
 #' \code{'mouse'}, \code{'pig'}, \code{'rat'}, \code{'rhesus'},
 #' \code{'worm'}, \code{'xenopus'}, \code{'yeast'} or \code{'zebrafish'}.
-#' @param measure Default is \code{'Resnik'}, could be one of \code{'Resnik'}, \code{'Lin'}, \code{'Rel'}, \code{'Jiang'} or \code{'Wang'}.
-#' @param combine Default is \code{'BMA'}, could be one of \code{'max'}, \code{'average'}, \code{'rcmax'} or \code{'BMA'}
-#' for combining semantic similarity scores of multiple GO terms associated with protein.
+#' @param measure Default is \code{'Resnik'}, could be one of
+#' \code{'Resnik'}, \code{'Lin'}, \code{'Rel'}, \code{'Jiang'} or \code{'Wang'}.
+#' @param combine Default is \code{'BMA'}, could be one of
+#' \code{'max'}, \code{'average'}, \code{'rcmax'} or \code{'BMA'}
+#' for combining semantic similarity scores of multiple GO terms
+#' associated with protein.
+#'
 #' @return A n x n matrix.
 #'
 #' @keywords GO Ontology similarity
@@ -146,9 +172,8 @@ parGOSim = function (golist, type = c('go', 'gene'),
 #'
 #' @author Nan Xiao <\url{https://nanx.me}>
 #'
-#' @seealso See \code{\link{parGOSim}} for
-#' protein similarity calculation based on
-#' Gene Ontology (GO) semantic similarity.
+#' @seealso See \code{\link{parGOSim}} for protein similarity calculation
+#' based on Gene Ontology (GO) semantic similarity.
 #' See \code{\link{parSeqSim}} for paralleled protein similarity
 #' calculation based on Smith-Waterman local alignment.
 #'
@@ -159,37 +184,37 @@ parGOSim = function (golist, type = c('go', 'gene'),
 #' # Be careful when testing this since it involves GO similarity computation
 #' # and might produce unpredictable results in some environments
 #'
-#' require(GOSemSim)
-#' require(org.Hs.eg.db)
+#' library("GOSemSim")
+#' library("org.Hs.eg.db")
 #'
 #' # by GO terms
 #' go1 = c("GO:0004022", "GO:0004024", "GO:0004023")
 #' go2 = c("GO:0009055", "GO:0020037")
-#' gsim1 = twoGOSim(go1, go2, type = 'go', ont = 'MF', measure = 'Wang')
+#' gsim1 = twoGOSim(go1, go2, type = "go", ont = "MF", measure = "Wang")
 #' print(gsim1)
 #'
 #' # by Entrez gene id
-#' gene1 = '241'
-#' gene2 = '251'
-#' gsim2 = twoGOSim(gene1, gene2, type = 'gene', ont = 'BP', measure = 'Lin')
+#' gene1 = "241"
+#' gene2 = "251"
+#' gsim2 = twoGOSim(gene1, gene2, type = "gene", ont = "BP", measure = "Lin")
 #' print(gsim2)}
 
-twoGOSim = function (id1, id2, type = c('go', 'gene'),
-                     ont = 'MF', organism = 'human',
-                     measure = 'Resnik', combine = 'BMA') {
+twoGOSim = function(
+  id1, id2, type = c('go', 'gene'),
+  ont = 'MF', organism = 'human', measure = 'Resnik', combine = 'BMA') {
 
   if ( type == 'go' ) {
-    sim = GOSemSim::mgoSim(id1, id2,
-                           ont = ont, organism = organism,
-                           measure = measure, combine = combine)
+    sim = GOSemSim::mgoSim(
+      id1, id2, ont = ont, organism = organism,
+      measure = measure, combine = combine)
   }
 
   if ( type == 'gene' ) {
-    sim = GOSemSim::geneSim(id1, id2,
-                            ont = ont, organism = organism,
-                            measure = measure, combine = combine)$geneSim
+    sim = GOSemSim::geneSim(
+      id1, id2, ont = ont, organism = organism,
+      measure = measure, combine = combine)$geneSim
   }
 
-  return(sim)
+  sim
 
 }
