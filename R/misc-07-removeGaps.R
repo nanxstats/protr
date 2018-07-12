bd1e99d5fbcaf3229f08fd0bcd4e4d2e38d1594e
Nan Xiao <road2stat@gmail.com>
2018-07-12 14:50:00 -0400

    add new function removeGaps

---
+++ R/misc-07-removeGaps.R
@@ -0,0 +1,37 @@
+#' Remove or replace gaps from protein sequences.
+#'
+#' Remove/replace gaps or any irregular characters from protein sequences,
+#' to make them suitable for feature extraction or sequence alignment
+#' based similarity computation.
+#'
+#' @param x character vector, containing the input protein sequence(s).
+#' @param pattern character string contains the gap (or other irregular)
+#' character to be removed or replaced. Default is \code{"-"}.
+#' For advanced usage, see \code{\link{gsub}}.
+#' @param replacement a replacement for matched characters.
+#' Default is \code{""} (remove the matched character).
+#' @param ... addtional parameters for \code{\link{gsub}}.
+#'
+#' @return a vector of protein sequence(s) with gaps or
+#' irregular characters removed/replaced.
+#'
+#' @author Nan Xiao <\url{https://nanx.me}>
+#'
+#' @export removeGaps
+#'
+#' @examples
+#' # sequences that contain gaps ("-")
+#' aaseq <- list(
+#'   "MHGDTPTLHEYMLDLQPETTDLYCYEQLSDSSE-EEDEIDGPAGQAEPDRAHYNIVTFCCKCDSTLRLCVQSTHVDIRTLEDLLMGTLGIVCPICSQKP",
+#'   "MHGDTPTLHEYMLDLQPETTDLYCYEQLNDSSE-EEDEIDGPAGQAEPDRAHYNIVTFCCKCDSTLRLCVQSTHVDIRTLEDLLMGTLGIVCPICSQKP"
+#' )
+#'
+#' # gaps create issues for alignment computation
+#' parSeqSim(aaseq)
+#'
+#' # remove the gaps and redo the alignment
+#' nogapseq <- removeGaps(aaseq)
+#' parSeqSim(nogapseq, cores = 1)
+
+removeGaps <- function(x, pattern = "-", replacement = "", ...)
+  gsub(pattern, replacement, x, ...)
